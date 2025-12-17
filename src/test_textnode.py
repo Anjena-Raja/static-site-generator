b3cb5ba4447280e8a1291c98bbcc1372601c87ac
Anjena-Raja <anjena.raja@gmail.com>
2025-12-17 15:27:34 -0800

    Conversion from TextNode to HTMLNode implemented

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -37,7 +37,45 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK, 'https://www.boot.dev')
         self.assertEqual(node.__repr__(), 'TextNode(This is a text node, link, https://www.boot.dev)')
     
-
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_text_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'b')
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_text_italics(self):
+        node = TextNode("This is a text node", TextType.ITALICS)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'i')
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_text_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'code')
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_text_link(self):
+        node = TextNode("This is a text node", TextType.LINK, 'https://www.google.com')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'a')
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props['href'], 'https://www.google.com')
+    
+    def test_text_image(self):
+        node = TextNode("This is a text node", TextType.IMAGE, 'https://www.boot.dev/img/bootdev-logo-full-small.webp')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'img')
+        self.assertEqual(html_node.value, '')
+        self.assertEqual(html_node.props['src'], 'https://www.boot.dev/img/bootdev-logo-full-small.webp')
+        self.assertEqual(html_node.props['alt'], "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,12 @@
 from enum import Enum
+import htmlnode
 
 class TextType(Enum):
+    TEXT = 'text'
     PLAIN = 'plain'
     BOLD = 'bold'
     ITALICS = 'italics'
+    CODE = 'code'
     LINK = 'link'
     IMAGE = 'image'
 
@@ -19,3 +22,33 @@ class TextNode:
     def __repr__(self) -> str:
         return f'TextNode({self.text}, {self.type.value}, {self.url})'
     
+def text_node_to_html_node(text_node) -> htmlnode.LeafNode:
+    match text_node.type:
+        case TextType.TEXT:
+            return htmlnode.LeafNode(tag=None, value=text_node.text)
+        case TextType.BOLD:
+            return htmlnode.LeafNode(tag='b', value=text_node.text)
+        case TextType.ITALICS:
+            return htmlnode.LeafNode(tag='i', value=text_node.text)
+        case TextType.CODE:
+            return htmlnode.LeafNode(tag='code', value=text_node.text)
+        case TextType.LINK:
+            return htmlnode.LeafNode(tag='a', value=text_node.text, props={'href': text_node.url})
+        case TextType.IMAGE:
+            return htmlnode.LeafNode(tag='img', value='', props={'src': text_node.url, 'alt': text_node.text})
+    raise ValueError('Invalid TextNode specifications')
+        
+        
+
+
+'''
+It should handle each type of the TextType enum. If it gets a TextNode that is none of those types, it should raise an exception. 
+Otherwise, it should return a new LeafNode object.
+
+TextType.TEXT: This should return a LeafNode with no tag, just a raw text value.
+TextType.BOLD: This should return a LeafNode with a "b" tag and the text
+TextType.ITALIC: "i" tag, text
+TextType.CODE: "code" tag, text
+TextType.LINK: "a" tag, anchor text, and "href" prop
+TextType.IMAGE: "img" tag, empty string value, "src" and "alt" props ("src" is the image URL, "alt" is the alt text)
+'''
